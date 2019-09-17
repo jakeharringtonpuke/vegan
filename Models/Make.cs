2ebcb96723ba6a9f18f46ce70ac045039ce3c582
jakeharringtonpuke <jakeharringtonpuke@gmail.com>
2019-09-17 18:07:16 -0400

    Created Initial Domain Model Models added:- 1- Model 2- Make added entityframework.SqlServer

---
+++ Models/Make.cs
@@ -0,0 +1,18 @@
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+namespace vegan.Models
+{
+    public class Make
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<Model> Models { get; set; }
+
+        public Make()
+        {
+            Models = new Collection<Model>();
+        }
+    }
+}
